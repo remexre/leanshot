560c4c770e515bded27d0e52d1a42f65af39883e
Nathan Ringo <remexre@gmail.com>
2018-09-26 16:19:23 -0500

    Adds Justfile, builds bindgen bindings based on system.

--- imlib2-sys/build.rs
+++ imlib2-sys/build.rs
@@ -1,3 +1,18 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+use bindgen::Builder;
+
 fn main() {
     println!("cargo:rustc-link-lib=Imlib2");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    Builder::default()
+        .header("wrapper.h")
+        .blacklist_type("max_align_t")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to write bindings");
 }
